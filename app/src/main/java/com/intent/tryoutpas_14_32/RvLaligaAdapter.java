d84c7d54af3281a09d3e2acced1bab0a0613eac3
Petrusgld <coysoti@gmail.com>
2025-05-15 13:05:03 +0700

    feat [AM] laliga-page

--- app/src/main/java/com/intent/tryoutpas_14_32/RvLaligaAdapter.java
+++ app/src/main/java/com/intent/tryoutpas_14_32/RvLaligaAdapter.java
@@ -1,4 +1,63 @@
 package com.intent.tryoutpas_14_32;
 
-public class RvLaligaAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class RvLaligaAdapter extends RecyclerView.Adapter<RvLaligaAdapter.ViewHolder> {
+
+    List<ModelClass> teamList;
+
+    public RvLaligaAdapter(LaligaFragment laligaList, List<ModelClass> teamList) {
+        this.teamList = teamList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ModelClass team = teamList.get(position);
+        holder.tvTeam.setText(team.getStrTeam());
+        holder.tvStadium.setText(team.getStrStadium());
+
+        Glide.with(holder.itemView.getContext())
+                .load(team.getStrBadge())
+                .into(holder.ivBadge);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return teamList.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTeam, tvStadium;
+        ImageView ivBadge;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTeam = itemView.findViewById(R.id.tvTeam);
+            tvStadium = itemView.findViewById(R.id.tvStadium);
+            ivBadge = itemView.findViewById(R.id.ivBadge);
+
+        }
+    }
 }
